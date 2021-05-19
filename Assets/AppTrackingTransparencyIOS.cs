59ea25328161164b95f0735f401b9387be69cb7b
Vasilii Manoilov <wmadwand@gmail.com>
2021-05-19 12:27:01 +0300

    Add AppTrackingTransparencyIOS script

---
+++ Assets/AppTrackingTransparencyIOS.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+#if UNITY_IOS
+
+using Unity.Advertisement.IosSupport;
+
+#endif
+
+public class AppTrackingTransparencyIOS : MonoBehaviour
+{
+    private IEnumerator Start()
+    {
+        yield return RequestPermission();
+
+        Debug.Log("SUCCESS !!!");
+    }
+
+    public IEnumerator RequestPermission()
+    {
+#if UNITY_IOS
+        var currentStatus = ATTrackingStatusBinding.GetAuthorizationTrackingStatus();
+        Debug.Log(string.Format("Current authorization status: {0}", currentStatus.ToString()));
+
+        if (currentStatus != ATTrackingStatusBinding.AuthorizationTrackingStatus.AUTHORIZED)
+        {
+            Debug.Log("Requesting authorization...");
+            ATTrackingStatusBinding.RequestAuthorizationTracking();
+            //_coroutiner.StartCoroutine(RequestCallbackAwaiter(_callback));
+        }
+
+        yield return WaitForAuthorized();
+#else 
+        yield return null;
+#endif
+    }    
+
+#if UNITY_IOS
+    private IEnumerator WaitForAuthorized()
+    {
+        yield return new WaitUntil(() => ATTrackingStatusBinding.GetAuthorizationTrackingStatus() == ATTrackingStatusBinding.AuthorizationTrackingStatus.AUTHORIZED);
+    }
+
+    private IEnumerator RequestCallbackAwaiter(Action callback)
+    {
+        yield return new WaitUntil(() => ATTrackingStatusBinding.GetAuthorizationTrackingStatus() == ATTrackingStatusBinding.AuthorizationTrackingStatus.AUTHORIZED);
+        callback();
+    }
+#endif
+
+    public void Request()
+    {
+        ATTrackingStatusBinding.RequestAuthorizationTracking();
+    }
+}
